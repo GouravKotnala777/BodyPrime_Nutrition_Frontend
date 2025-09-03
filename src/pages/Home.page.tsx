51d5e93d4517789e179b801be90231ab0132fd58
gouravkotnala <gouravkotnala777@gmail.com>
2025-09-03 09:18:27 +0530

    feat(Home.page.tsx) : add home page skeleton

---
+++ src/pages/Home.page.tsx
@@ -0,0 +1,13 @@
+
+
+function Home() {
+    
+    return(
+        <section>
+            <h1>This is Home Page</h1>
+            <p>this is nothing but a paragraph.</p>
+        </section>
+    )
+};
+
+export default Home;
