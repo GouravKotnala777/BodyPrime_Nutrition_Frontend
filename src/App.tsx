22c6c00cdf05106ce58432eff00bf5582cea5da2
gouravkotnala <gouravkotnala777@gmail.com>
2025-09-03 09:13:30 +0530

    feat(App.tsx) : add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,20 @@
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home.page';
+import Header from './components/Header.component';
 
 function App() {
 
   return (
-    <>
-    <header
-      className="border-2 border-violet-500 flex justify-between items-center"
-    >
-      <section className="border-2 border-indigo-500 ">
-        <img className="w-[60px] h-[60px]" src={viteLogo} alt={viteLogo} />
-      </section>
-      <section
-        className="border-2 border-blue-500 "
-      >
-        <nav className="border-2 border-green-500 flex w-[500px] justify-around">
-          <div className="nav_item">Home</div>
-          <div className="nav_item">Profile</div>
-          <div className="nav_item">Orders</div>
-          <div className="nav_item">Register</div>
-          <div className="nav_item">Login</div>
-        </nav>
-      </section>
-    </header>
-    <main>
-      <h1>Hello</h1>
-      <p>my name is gourav</p>
+    <BrowserRouter>
+    <Header />
+    <main className="border-2 border-red-500">
+      <Routes>
+        <Route path={"/home"} element={<Home />} />
+      </Routes>
     </main>
     <footer></footer>
-    </>
+    </BrowserRouter>
   )
 }
 
