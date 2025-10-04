b135924efc48fdea97d86b33768f8a8a35acbc02
gouravkotnala <gouravkotnala777@gmail.com>
2025-10-04 15:52:00 +0530

    feat : add updateOrder api handler

--- src/apis/order.api.ts
+++ src/apis/order.api.ts
@@ -1,5 +1,5 @@
 import { apiHandler } from "../utils/functions";
-import type { CreateOrderFormType, OrderTypes } from "../utils/types";
+import type { CreateOrderFormType, OrderTypes, PaymentStatusType } from "../utils/types";
 
 
 
@@ -8,6 +8,21 @@ export async function createOrder(body:CreateOrderFormType) {
         const res = await apiHandler<CreateOrderFormType, {clientSecret:string; newOrder:OrderTypes;}>({
             endpoint:"/order/create",
             method:"POST",
+            contentType:"application/json",
+            body
+        });
+        return res;        
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
+
+export async function updateOrder(orderID:string, body:{transactionID:string; status:PaymentStatusType; message:string|null; error?:string;}) {
+    try {
+        const res = await apiHandler<{transactionID:string; status:PaymentStatusType; message:string|null; error?:string;}, {}>({
+            endpoint:`/order/update?orderID=${orderID}`,
+            method:"PUT",
             contentType:"application/json",
             body
         });
