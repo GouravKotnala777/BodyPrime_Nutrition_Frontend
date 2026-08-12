f26e58788daa0fea2e043c6fa59e9abc9ba94035
gouravkotnala <gouravkotnala777@gmail.com>
2026-08-12 17:11:29 +0530

    feat(Skeletan.tsx) : add Skeletan.tsx component

---
+++ src/components/Skeletan.tsx
@@ -0,0 +1,18 @@
+
+
+function Skeletan() {
+    
+    return(
+        <div className="w-full h-full"
+            style={{
+                background:"linear-gradient(90deg, var(--color-gray-400) 0% 30%, var(--color-gray-300) 53% 57%, var(--color-gray-400) 70% 100%)",
+                backgroundSize:"200% 100%",
+                animation:"animate-skeletan 4s linear infinite"
+            }}        
+        >
+
+        </div>
+    )
+};
+
+export default Skeletan;
