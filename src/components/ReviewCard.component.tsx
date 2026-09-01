10725f7903f5e7c0cdb590c8832faae8742f0229
gouravkotnala <gouravkotnala777@gmail.com>
2026-09-01 20:03:46 +0530

    refactor(ReviewCard.component.tsx) : update styles

--- src/components/ReviewCard.component.tsx
+++ src/components/ReviewCard.component.tsx
@@ -1,4 +1,3 @@
-import { NavLink } from "react-router-dom";
 import RatingStars from "./RatingStars.component";
 import type { ReviewTypesPopulated } from "../utils/types";
 
@@ -8,27 +7,27 @@ function ReviewCard({productID, userID, ...review}:ReviewTypesPopulated) {
     const reviewActionDate = review.updatedAt ? `Review updated on ${new Date(review.updatedAt).toLocaleString(undefined, {day:"2-digit", month:"short", year:"numeric"})}`:`Reviewed in India on ${new Date(review.createdAt).toLocaleString(undefined, {day:"numeric", month:"short", year:"numeric"})}`;
 
     return(
-        <div className="flex flex-col gap-2 py-5 border-b-[1px] border-b-gray-200">
-            <div className="flex">
-                <img src="/vite.svg" alt="/vite.svg" />
+        <div className="border border-gray-200 flex flex-col gap-2 w-80 text-center rounded-md p-4">
+            <div className="flex flex-col items-center gap-2">
+                <img src="/vite.svg" alt="/vite.svg" className="border border-gray-200 rounded-full size-14" />
                 <span>{userID.name}</span>
             </div>
-            <div className="flex">
+            <div className="flex justify-center">
                 <RatingStars rating={review.rating} outOf={5} />
                 &nbsp;&nbsp;&nbsp; <span className="text-orange-400 font-semibold">{review.isVerifiedPurchase&&"Verified Purchase"}</span>
             </div>
             <div className="flex flex-col text-gray-500">
                 <span>{reviewActionDate}</span>
-                <span>Flavour Name: {productID.flavor} {productID.size} {productID.weight}</span>
+                <span>Flavour Name: {productID.flavor} {productID.weight}</span>
             </div>
-            <div className="flex">
+            <div className="flex text-justify">
                 <span>{review.comment}</span>
             </div>
-            <div className="flex justify-between">
+            {/*<div className="flex justify-between">
                 <button className="border-[1px] px-4 py-2 rounded-3xl text-gray-900">Helpful</button>
                 <NavLink to="/share" className="px-4 py-2 text-gray-900">Share</NavLink>
                 <NavLink to="/report" className="px-4 py-2 text-gray-900">Report</NavLink>
-            </div>
+            </div>*/}
         </div>
     )
 };
