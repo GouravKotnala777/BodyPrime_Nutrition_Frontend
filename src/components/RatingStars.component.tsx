80d6aaab7c1bcffb05ce75c2ae3b77d8eb30d70d
gouravkotnala <gouravkotnala777@gmail.com>
2026-09-03 22:30:54 +0530

    style(RatingStars.component.tsx) : add prop for starts size

--- src/components/RatingStars.component.tsx
+++ src/components/RatingStars.component.tsx
@@ -1,13 +1,17 @@
 import { IoIosStar, IoIosStarHalf, IoIosStarOutline } from "react-icons/io";
 
 
-function RatingStars({rating, outOf}:{rating:number; outOf:number;}) {
+function RatingStars({rating, outOf, starSize="14px"}:{rating:number; outOf:number; starSize?:string;}) {
     const fullStars = Math.floor(rating);
     const hasHalf = rating % 1 >= 0.5;
     const emptyStar = outOf - fullStars - (hasHalf?1:0);
 
     return(
-        <div className="flex text-[1.4rem] text-yellow-500">
+        <div className="flex text-yellow-500"
+            style={{
+                fontSize:starSize
+            }}
+        >
             {
                 Array.from({length:fullStars}).map((_, index) => (
                     <span key={index}><IoIosStar /></span>
