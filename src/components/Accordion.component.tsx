48ce1c77315b6bef75f1cb53235fe6449f78ff0c
gouravkotnala <gouravkotnala777@gmail.com>
2026-09-07 22:57:38 +0530

    style(Accordion.component.tsx) : add styles and chevrons instead of up/down arrows

--- src/components/Accordion.component.tsx
+++ src/components/Accordion.component.tsx
@@ -1,11 +1,23 @@
-import { useState } from "react";
-import {BsArrowUpShort} from "react-icons/bs";
+import { useState, type ReactNode } from "react";
 
+//interface AccordionProptypes{
+//    data:{heading:string; para:string}[];
+//};
 interface AccordionProptypes{
-    data:{tags:string[]; heading:string; para:string}[];
+    data:{heading:ReactNode; para:ReactNode}[];
+    chevronSize?:"xs"|"sm"|"md"|"lg"|"xl";
 };
 
-function Accordion({data}:AccordionProptypes) {
+const applyChevronSize = {
+    xs:"size-2.5",
+    sm:"size-3",
+    md:"size-3.5",
+    lg:"size-4",
+    xl:"size-4.5",
+};
+
+
+function Accordion({data, chevronSize="md"}:AccordionProptypes) {
     const [activeCords, setActiveCords] = useState<Record<string, boolean>>({});
     
 
@@ -13,12 +25,34 @@ function Accordion({data}:AccordionProptypes) {
         <div className="transition-all ease-in-out duration-300">
             {
                 data.map(({heading, para}, index) => (
-                    <div key={index} className={`transition-all ease-in-out duration-300`}>
-                        <div className="text-gray-700 p-3 flex justify-between items-center cursor-pointer transition-all ease-in-out duration-300" onClick={()=>setActiveCords((prev)=>({...prev, [`${index}`]:!prev[`${index}`]}))}>
-                            <div className="text-lg sm:text-xl">{heading}</div>
-                            <div className={`text-2xl ${activeCords[`${index}`]?"rotate-0":"rotate-180"} transition-transform ease-in-out duration-300`}><BsArrowUpShort /></div>
+                    <div key={index} className="">
+                        <div className="text-gray-700 flex justify-between items-center cursor-pointer hover:bg-primary-50" onClick={()=>setActiveCords((prev)=>({...prev, [`${index}`]:!prev[`${index}`]}))}>
+                            {heading}
+
+                            <div className="text-gray-500 mr-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor"
+                                className={`${activeCords[index]?"rotate-x-180":"rotate-x-0"} translate-y-0.5 ${applyChevronSize[chevronSize]} origin-center transition-transform ease-in-out duration-400`}
+                                >
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
+                                </svg>
+
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor"
+                                className={`${activeCords[index]?"rotate-x-180":"rotate-x-0"} -translate-y-0.5 ${applyChevronSize[chevronSize]} origin-center transition-transform ease-in-out duration-400`}
+                                >
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                                </svg>
+                            </div>
                         </div>
-                        <div className={`text-gray-600 bg-primary-50 text-lg px-4 [text-shadow:0px_0px_2px_var(--color-gray-300)] tracking-wide text-left overflow-hidden rounded-lg origin-top [box-shadow:0px_0px_4px_0px_var(--primary-300)_inset] ${activeCords[`${index}`]?"h-60 py-2":"h-0 py-0"} transition-all ease-in-out duration-300`}>{para}</div>
+                        <div className={`text-gray-600 bg-primary-50 text-lg
+                            [text-shadow:0px_0px_2px_var(--color-gray-300)] [box-shadow:0px_0px_4px_0px_var(--primary-300)_inset]
+                            tracking-wide text-left rounded-lg origin-top grid
+                            ${activeCords[`${index}`]?"grid-rows-[1fr]":"grid-rows-[0fr]"}
+                            transition-[grid-template-rows] ease-in-out duration-400
+                        `}>
+                            <div className="overflow-hidden">
+                                {para}
+                            </div>
+                        </div>
                     </div>
                 ))
             }
