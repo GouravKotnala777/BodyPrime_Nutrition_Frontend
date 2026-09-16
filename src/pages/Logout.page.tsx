b50f08c7777b58fb5bc18dbaf8da7a465560e286
gouravkotnala <gouravkotnala777@gmail.com>
2026-09-16 18:16:25 +0530

    refactor(Register.page.tsx & Login.page.tsx & Logout.page.tsx) : update ui and form validation

--- src/pages/Logout.page.tsx
+++ src/pages/Logout.page.tsx
@@ -1,27 +1,64 @@
+import { useState } from "react";
 import { logout } from "../apis/user.api";
+import Spinner from "../components/Spinner.component";
+import toast from "react-hot-toast";
 
 
 
 function Logout() {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [isConfirmed, setIsConfirmed] = useState<boolean>(false);
+
 
     async function logoutHandler() {
-        const res = await logout();
-
-        console.log(res);
+        setIsLoading(true);
         
+        setTimeout(async() => {
+            if (!isConfirmed) {
+                toast.error("Please check the confirmation field first", {position:"top-center", duration:3000});
+                setIsLoading(false);
+                return;
+            }
+            const res = await logout();
+            
+            if (res.success) {
+                window.location.href = "/";
+            }
+            setIsLoading(false);
+        }, 1000);
     };
     
     return(
-        <section className="max-w-2xl mx-auto flex flex-col items-center gap-10 my-10">
-            <div className="text-lg sm:text-2xl text-gray-800 font-bold text-center py-2 sm:py-4">
+        <section className="max-w-lg mx-auto flex flex-col items-center gap-10 my-10">
+            <div className="text-lg sm:text-2xl text-gray-800 font-bold text-center mt-10 py-2 sm:py-4">
                 <div>Logout Page</div>
             </div>
-            <div className="flex flex-col w-full gap-10">
+            <div className="flex flex-col w-full gap-15">
                 <div className="flex gap-2 text-xl mx-auto">
-                    <input name="confirmation" type="checkbox" className="w-[1.2rem]" />
+                    <input name="confirmation" type="checkbox" className="w-[1.2rem]" checked={isConfirmed} onChange={(e) => setIsConfirmed(e.target.checked)} />
                     <p className="">Do your really want to logout?</p>
                 </div>
-                <button className="text-xl bg-primary-400 text-white py-2 rounded-2xl font-semibold hover:opacity-80" onClick={logoutHandler}>Logout</button>
+                <div className="flex items-center gap-4 w-full sm:w-60 h-10 mx-auto">
+                    <button
+                        className={`
+                            border
+                            border-primary-300 bg-primary-200 text-primary-800 pb-0.75 hover:opacity-50
+                            text-center content-center h-full w-full rounded-lg
+                            transition-all ease-in-out duration-300
+                        `}
+                        
+                        onClick={logoutHandler}
+                    >
+                        {
+                            isLoading ?
+                                <div className="w-max mx-auto">
+                                    <Spinner color="var(--color-primary-800)" type="secondary" />
+                                </div>
+                                :
+                                <span>Logout</span>
+                        }
+                    </button>
+                </div>
 
             </div>
         </section>
--- src/pages/Register.page.tsx
+++ src/pages/Register.page.tsx
@@ -3,71 +3,123 @@ import { NavLink } from "react-router-dom";
 import { type RegisterFormTypes } from "../utils/types";
 import { register } from "../apis/user.api";
 import Spinner from "../components/Spinner.component";
+import toast from "react-hot-toast";
 
 
 function Register() {
     const [formData, setFormData] = useState<RegisterFormTypes>({name:"", email:"", mobile:"", gender:"male", password:""});
     const [isProcessing, setIsProcessing] = useState<boolean>(false);
+    const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
 
 
     function onChangeHandler(e:ChangeEvent<HTMLInputElement|HTMLSelectElement>) {
         setFormData({...formData, [e.target.name]:e.target.value});        
     };
     async function onClickHandler() {
+        setIsProcessing(true);
         try {
-            setIsProcessing(true);
-            const res = await register(formData);
-    
-            console.log(res);
+            if (!formData.name || !formData.email || !formData.password || !formData.gender || !formData.mobile) {
+                toast.error("all fields are required", {position:"top-center", duration:3000});
+                throw new Error("all fields are required");
+            }
+            if (!formData.email.includes("@") || !formData.email.includes(".")) {
+                toast.error("invalid email", {position:"top-center", duration:3000});
+                throw new Error("invalid email");
+            }
+            setTimeout(async() => {
+                const res = await register(formData);
+                console.log(res);
+                setIsProcessing(false);
+            }, 1500);
+
         } catch (error) {
             console.log(error);
-        }
-        finally{
-            setIsProcessing(false);
         }
     };
 
     return(
-        <section className="Register_section flex flex-col items-center gap-10 p-10">
-            <h1 className="text-4xl font-bold">Register Page</h1>
-            <label>
-                <input type="text" name="name" placeholder="Full Name" className="p-2 bg-amber-100" onChange={onChangeHandler} />
-            </label>
-            <label>
-                <input type="text" name="email" placeholder="Email" className="p-2 bg-amber-100" onChange={onChangeHandler} />
-            </label>
-            <label>
-                <input type="text" name="mobile" placeholder="Mobile" className="p-2 bg-amber-100" onChange={onChangeHandler} />
-            </label>
-            <label>
-                <input type="text" name="password" placeholder="Password" className="p-2 bg-amber-100" onChange={onChangeHandler} />
-            </label>
-
-            <label className="p-2 bg-amber-100">
-                <select name="gender" onChange={onChangeHandler}>
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
-                    <option value="other">Other</option>
-                </select>
-            </label>
-
-            <button className="p-2 bg-black text-white px-8 rounded-[8px]" onClick={onClickHandler}>{isProcessing?<Spinner color="white" width="20px" />:"Register"}</button>
-
-            <section className="w-full">
-            <div className="flex justify-between items-center">
-                <span className="border-[1px] border-gray-600 w-[40%]"></span>
-                <div>Or</div>
-                <span className="border-[1px] border-gray-600 w-[40%]"></span>
+        <section className="border border-gray-200 max-w-2xl mx-auto flex flex-col items-center gap-10 mt-25 mb-10 p-10 rounded-md">
+            <h1 className="text-xl font-bold">Register Page</h1>
+            <div className="w-full max-w-xs">
+                <input type="text" name="name" placeholder="Full Name"
+                    className="border border-primary-200 bg-primary-50 text-gray-700 w-full px-3 py-2 rounded-md"
+                    onChange={onChangeHandler}
+                />
+            </div>
+            <div className="w-full max-w-xs">
+                <input type="text" name="email"
+                    placeholder="Email"
+                    className="border border-primary-200 bg-primary-50 text-gray-700 w-full px-3 py-2 rounded-md"
+                    onChange={onChangeHandler}
+                />
+            </div>
+            <div className="w-full max-w-xs">
+                <input type="text" name="mobile" placeholder="Mobile"
+                    className="border border-primary-200 bg-primary-50 text-gray-700 w-full px-3 py-2 rounded-md"
+                    onChange={onChangeHandler}
+                />
+            </div>
+            <select name="gender" className="border border-primary-200 bg-primary-50 text-gray-700 w-full max-w-xs px-3 py-2 rounded-md" onChange={onChangeHandler}>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
+            </select>
+            <div className="border border-primary-200 w-full max-w-xs flex rounded-md overflow-hidden">
+                <input type={isPasswordVisible?"text":"password"} name="password"
+                    placeholder="Password"
+                    className="bg-primary-50 text-gray-700 w-full px-3 py-2 rounded-l-md"
+                    onChange={onChangeHandler}
+                />
+                <button className="w-10 h-full bg-primary-200 text-primary-800 px-3 py-2 relative hover:opacity-50 transition-all ease-in-out duration-300"
+                    onClick={()=>setIsPasswordVisible(!isPasswordVisible)}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={`size-6 -translate-x-1 ${isPasswordVisible?"blur-0 opacity-100 scale-100":"blur-sm opacity-0 scale-50"} transition-all ease-in-out duration-300`}>
+                        <path strokeLinecap="round" pathLength="1" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                    </svg>
+                    
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={`absolute top-[50%] left-[50%] -translate-[50%] size-6 ${isPasswordVisible?"blur-sm opacity-0 scale-50":"blur-0 opacity-100 scale-100"} transition-all ease-in-out duration-300`}>
+                        <path strokeLinecap="round" pathLength="1" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                        <path strokeLinecap="round" pathLength="1" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+                </button>
+            </div>
+            <div className="w-full max-w-xs h-10">
+                <button
+                    className={`
+                        border
+                        border-primary-300 bg-primary-200 text-primary-800 pb-0.75 hover:opacity-50
+                        text-center content-center h-full w-full rounded-lg
+                        transition-all ease-in-out duration-300
+                    `}
+                    
+                    onClick={onClickHandler}
+                >
+                    {
+                        isProcessing ?
+                            <div className="w-max mx-auto">
+                                <Spinner color="var(--color-primary-800)" type="secondary" />
+                            </div>
+                            :
+                            <span>Register</span>
+                    }
+                </button>
             </div>
 
-            <section className="flex justify-between">
-                <NavLink to={"/user/forget_password"} className="underline underline-offset-3">Forget password</NavLink>
-                <div>
-                    <span>don't have acc </span>
-                    <NavLink to={"/register"} className="underline underline-offset-3">Register</NavLink>
+            <div className="w-full">
+                <div className="flex justify-between items-center">
+                    <span className="border-[1px] border-gray-200 w-[40%]"></span>
+                    <div>Or</div>
+                    <span className="border-[1px] border-gray-200 w-[40%]"></span>
                 </div>
-            </section>
-            </section>
+
+                <div className="flex justify-between">
+                    <NavLink to={"/user/forget_password"} className="underline underline-offset-3">Forget password</NavLink>
+                    <div>
+                        <span>already have acc </span>
+                        <NavLink to={"/login"} className="underline underline-offset-3">Login</NavLink>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 };
